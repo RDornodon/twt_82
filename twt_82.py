a792c54dcf903ea8128644b773e9f1dc2f8dd5b8
RDornodon <>
2021-12-01 18:00:21 +0100

    Some refactor

--- twt_82.py
+++ twt_82.py
@@ -1,18 +1,18 @@
 class K:
- def __init__(S,r,c):S.r,S.c=r,c
- def D(S,O):R=abs(S.r-O.r)+abs(S.c-O.c);return R
-class S(K):
- def D(S,O):R=(4-O.r)+abs(O.c-max(7,min(3,O.c)));return R
+ def __init__(S,r,c,C):S.r,S.c,S.C=r,c,C
+ def D(S,O):
+  if('0'>S.C)>S.c:S.c=min(7,max(3,O.c))
+  R=S.c
+  if'0'>O.C:O.c=min(7,max(3,S.c))
+  return abs(S.r-O.r)+abs(R-O.c)
 E=enumerate
-if "_K" not in globals():
- global _K
- _K={C:K(r,c)for r,R in E(['1234567890','qwertyuiop','Casdfghjkl','**zxcvbnm'])for c,C in E(R)}|{' ':S(4,5)}
-
+_K={C:K(r,c,C)for r,R in E(['1234567890','qwertyuiop','Casdfghjkl','**zxcvbnm'])for c,C in E(R)}
 for I in[I:=input]*int(I()):
  s,U='',0
+ _K[' ']=K(4,0,' ')
  for C in I():
   if(C<(c:=C.lower()))!=U and'9'<C:s+='C';U^=1
-  s+=c
+  if s[-1:]!=c:s+=c
  m,r=int(I()),0
  for b,e in zip(s,s[1:]):
   r+=_K[b].D(_K[e])
